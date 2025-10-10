ce5e0c07b4a6298f10e5c753d2205970d4c0c6a4
Junecran <elliecraner0@gmail.com>
2025-10-10 01:26:15 +0100

    ufinished

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -1,22 +1,29 @@
 function setup() {
   createCanvas(640, 480);
-  background(230);
-  // noStroke();              
+  background(400);
+
 }
 
 function draw() {
-  fill(0);
-  rect(0, 0, 100, 100); // x, y, w, h
+  // the-computer.
+  quad(260, 300, 400, 305, 390, 200, 250, 200)
+  quad(262, 300, 350, 320, 490, 320, 400, 300)
 
-  fill(64);
-  rect(50, 50, 100, 100);
+  // left-arm.
+  arc(290, 198, 50, 60, PI, PI + QUARTER_PI);
+  arc(295, 180, 50, 10, PI, PI + QUARTER_PI);
 
-  fill(128)
-  rect(100, 100, 100, 100);
+  // right-arm.
+  arc(400, 190, 40, 20, PI + QUARTER_PI, TWO_PI);
+  arc(413, 190, 15, 120, 0, HALF_PI, PI);
+  // bottom-right-arm.
+  arc(408, 252, 20, 20, PI + QUARTER_PI, TWO_PI);
+  arc(398, 250, 40, 40, 0, HALF_PI, PI);
 
-  fill(196);
-  rect(150, 150, 200, 200);
-
-  fill(220);
-  rect(300, 300, 400, 400);
+  // top-face.
+  arc(340, 175, 90, 82, PI, PI + HALF_PI);
+  arc(340, 175, 90, 82, PI + QUARTER_PI, TWO_PI);
+  // bottom-face.
+  arc(310, 175, 30, 50, HALF_PI, PI);
+  arc(375, 175, 20, 50, 0, HALF_PI);
 }
