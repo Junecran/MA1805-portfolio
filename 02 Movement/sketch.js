8d5b0b2d99c24f89134d6491d7f7a5d347ab104a
Junecran <elliecraner0@gmail.com>
2025-10-13 16:32:46 +0100

    Edits to readme

--- 02 Movement/sketch.js
+++ 02 Movement/sketch.js
@@ -1,7 +1,26 @@
+let x = 0;
+let y = 0;
+let lines = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  background(220);
+  //background(255, 0, 100);
+
+  let x1 = random(width)
+  let y1 = random(height);
+  let x2 = random(width);
+  let y2 = random(height);
+  let color = random();
+  stroke(color);
+  line(x1, y1, x2, y2);
+
+  let myText = "lines" +lines;
+  fill(0);
+  textSize(width/20);
+  textAlign(CENTER, CENTER);
+  text(myText, width/2, height/2);
+  lines++;
 }
