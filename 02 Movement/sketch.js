d752e17bda00fb828ba495186340cd44dda4908f
Junecran <elliecraner0@gmail.com>
2025-10-19 15:55:09 +0100

    StartNoisearc

--- 02 Movement/sketch.js
+++ 02 Movement/sketch.js
@@ -1,38 +1,57 @@
 function setup() {
-  createCanvas(500, 500);
-  background(400);
+  createCanvas(600, 500);
+  noFill();
 
 }
+
+
+   // Functions and Variables //
+
+let nscale = 0.05; // Noise scale
+let ns = 8; // Noise strength 
+
+//Noise arc function
+function noisyArc(x1, y1, x2, y2) {
+
+
+
+
+  
+}
+
+
+
 
 function draw() {
-// get rid of after //
-background(250);
-text(`x: ${mouseX} y: ${mouseY}`, 50, 50);
-stroke(1); 
-
-
-   // Trees Outline //
-
-// Tree far right
-noFill()
-
-line(410, 80, 420, 165);
-arc(501, 247, 200, 280, PI, PI + QUARTER_PI);
-arc(383, 248, 35, 20, 0, HALF_PI);
-arc(387, 263, 25, 10, PI, PI + QUARTER_PI);
-arc(405, 284, 10, 15, PI + QUARTER_PI, TWO_PI);
-arc(400, 265, 50, 30, HALF_PI, PI);
-arc(388, 288, 40, 70, 0, HALF_PI);
-arc(420, 332, 70, 40, HALF_PI, PI);
-line(421, 351, 492, 355);
-
-
-
-
-// Background trees
-
-
+  background(255);
+  
+  // Fornt right tree
+noiseboy(410, 80, 420, 165);
+noiseboyArc(501, 247, 200, 280, PI, PI + QUARTER_PI);
+noiseboyArc(383, 248, 35, 20, 0, HALF_PI);
+noiseboyArc(387, 263, 25, 10, PI, PI + QUARTER_PI);
+noiseboyArc(405, 284, 10, 15, PI + QUARTER_PI, TWO_PI);
+noiseboyArc(400, 265, 50, 30, HALF_PI, PI);
+noiseboyArc(388, 288, 40, 70, 0, HALF_PI);
+noiseboyArc(420, 332, 70, 40, HALF_PI, PI);
+noiseboyLine(421, 351, 492, 355);
 }
 
-// Tree Base Colour //
-//quad(410, 82, 500, 82, 500, 105, 420, 164, );
+// -------- Noisy Line Function --------
+function noiseboy(x1, y1, x2, y2) {
+  beginShape();
+  let steps = 100;
+  for (let i = 0; i <= steps; i++) {
+    let t = i / steps;
+    let x = lerp(x1, x2, t);
+    let y = lerp(y1, y2, t);
+
+    let n = noise(x * nscale, y * nscale, frameCount * 0.01);
+    let offsetX = map(n, 0, 1, -ns, ns);
+    let offsetY = map(n, 0, 1, -ns, ns);
+    
+    vertex(x + offsetX, y + offsetY);
+  }
+  endShape();
+}
+
