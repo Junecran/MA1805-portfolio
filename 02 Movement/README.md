2b489cad1b9c6acecfe0117bd99df9dbc902b60d
Junecran <elliecraner0@gmail.com>
2025-10-19 19:22:20 +0100

    Placinglines

--- 02 Movement/README.md
+++ 02 Movement/README.md
@@ -10,8 +10,18 @@
      text(`x: ${mouseX} y: ${mouseY}`, 50, 50);
      stroke(0); 
 
+**Differnet code for arcs instead of vertices**
+
+     Arc(501, 247, 200, 280, PI, PI + QUARTER_PI);
+     Arc(383, 248, 35, 20, 0, HALF_PI);
+     Arc(387, 263, 25, 10, PI, PI + QUARTER_PI);
+     Arc(405, 284, 10, 15, PI + QUARTER_PI, TWO_PI);
+     Arc(400, 265, 50, 30, HALF_PI, PI);
+     Arc(388, 288, 40, 70, 0, HALF_PI);
+     Arc(420, 332, 70, 40, HALF_PI, PI);
+
 ## References Used
 
  Used Throughout the code - https://p5js.org/reference
 
- To help create the 'noisyArc' and 'noisyLine' fucnction - https://happycoding.io/tutorials/processing/creating-functions
+ To help create the 'noisyVertex' and 'drawShape' fucnction - https://happycoding.io/tutorials/processing/creating-functions
--- 02 Movement/sketch.js
+++ 02 Movement/sketch.js
@@ -1,51 +1,90 @@
+
+   // Data // 
+// Customize Key // For easy access.
+let nScale = 0.05; // Noise smoothness
+let ns = 10; // Noise strength
+
+// Noise function 
+
+function drawLines(points) {
+  stroke(0);
+  for (let i = 0; i < points.length - 1; i++) {
+    let p1 = points[i];
+    let p2 = points[i + 1];
+
+    // Calculate noise offsets
+    let n1 = noise(p1[0] * nScale, p1[1] * nScale, frameCount * 0.01);
+    let n2 = noise(p2[0] * nScale, p2[1] * nScale, frameCount * 0.01);
+
+    let offX1 = map(n1, 0, 1, -ns, ns);
+    let offY1 = map(n1, 0, 1, -ns, ns);
+    let offX2 = map(n2, 0, 1, -ns, ns);
+    let offY2 = map(n2, 0, 1, -ns, ns);
+
+    // Draw line between jittered points
+    line(p1[0] + offX1, p1[1] + offY1, p2[0] + offX2, p2[1] + offY2);
+  }
+}
+// Tree Data 
+let frontRightTree = [
+[409, 84], [412, 105], [418, 150], [419, 170],
+[412, 180], [408, 188], [405, 196], [402, 202],
+[398, 213], [394, 225], [395, 235], [397, 251],
+[388, 261], [376, 270], [374, 274], [380, 277],
+[393, 273], [397, 272], [404, 274], [410, 285],
+[406, 302], [392, 319], [382, 335], [390, 349],
+[398, 349], [419, 347], [436, 348], [450, 350],
+[462, 356], [483, 356]
+];
+let forntLeftTree = [
+ [110, 341], [108, 333], [102, 332], [98, 334],
+ [93, 339], [87, 339], [82, 335], [85, 332],
+ [84, 328], [80, 327], [81, 322], [92, 322],
+ [97, 317], [97, 310], [94, 304], [85, 301],
+];
+let Tree2 = [
+[398, 230], [390, 233], [385, 236], [380, 241],
+[378, 249], [363, 249], [329, 253], [328, 261],
+[306, 266], [302, 274], [296, 274], [290, 280],
+[286, 281], [286, 296], [288, 305], [295, 309],
+[303, 303], [312, 300], [312, 296], [318, 290],
+[325, 288], [341, 288], [342, 294], [341, 298],
+[329, 307], [319, 318], [320, 322], [323, 324],
+[330, 334], [325, 344], [321, 348], [323, 355],
+[342, 359], [375, 357], [400, 358], [428, 358],
+[481, 361]
+];
+let backgroundTree = [
+[334, 85], [333,113], [332, 120], [334, 128],
+[336, 136], [332, 141], [320, 146], [313, 149],
+[311, 158], [306, 162], [304, 167], [302, 174],
+[315, 184], [313, 196], [310, 206], [306, 211],
+[305, 223], [301, 228], [292, 232], [289, 234],
+[282, 237], [281, 240], [286, 250], [290, 253],
+[303, 265]
+
+];
+
+
+
+
+
+
+
+   // Canvas //
 function setup() {
  createCanvas(600, 500);
- noFill();
 
 }
-
-   // Functions and Variables //
-
-let nScale = 0.05; // Noise scale
-let ns = 10; // Noise strength 
-
-// Noise arc function
-function noisyArc(cx, cy, w, h, startAngle, endAngle) {
-  beginShape();
-  for (let a = startAngle; a < endAngle; a += 0.05) {
-    let x = cx + cos(a) * (w / 2);
-    let y = cy + sin(a) * (h / 2);
-
-    let n = noise(x * nScale, y * nScale, frameCount * 0.01);
-    let offset = map(n, 0, 1, -ns, ns);
-    
-    // Offset perpendicular to the arc direction
-    let nx = x + cos(a + HALF_PI) * offset;
-    let ny = y + sin(a + HALF_PI) * offset;
-
-    vertex(nx, ny);
-  }
-  endShape();
-}
-
-
-  // Trees //
 
 function draw() {
- background(255);
-  
-// Fornt right tree
-noisyArc(501, 247, 200, 280, PI, PI + QUARTER_PI),
-noisyArc(383, 248, 35, 20, 0, HALF_PI),
-noisyArc(387, 263, 25, 10, PI, PI + QUARTER_PI),
-noisyArc(405, 284, 10, 15, PI + QUARTER_PI, TWO_PI),
-noisyArc(400, 265, 50, 30, HALF_PI, PI),
-noisyArc(388, 288, 40, 70, 0, HALF_PI),
-noisyArc(420, 332, 70, 40, HALF_PI, PI);
+  background(255);
+  text(`x: ${mouseX} y: ${mouseY}`, 100, 100);
 
-// Background trees
-noisyArc(, PI, PI + QUARTER_PI);
-
+drawLines(frontRightTree);
+drawLines(forntLeftTree);
+drawLines(Tree2);
+drawLines(backgroundTree);
 
 }
 
