93484cb468e88bb0f0855fe799cb0f8d9655c46c
Junecran <elliecraner0@gmail.com>
2025-10-20 16:06:51 +0100

    Before Image try

--- 03 Multiples/sketch.js
+++ 03 Multiples/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowHeight, windowWidth);
 }
+
 
 function draw() {
   background(220);
+
+
+  let i = 1;
+  while(i<=500) {
+    let r = random(30);
+    circle(i, i+r, 30+r);
+    i = i+3;
+  }
+
+  fill(0, 255, 0);
+  for(i=0; i<600; i=i+1.4){
+    let r = random(70);
+    circle(i+100, i, r);
+    
+  }
 }
+
